e092dd44ee0d86229af1f8f04b73b0fecb15c8b0
LUCIANO VILMAR DA SILVA <lvs.06.dev@gmail.com>
2025-08-27 22:23:16 -0300

    Mensagem do commit

--- ClientWalletProvider.tsx
+++ ClientWalletProvider.tsx
@@ -3,7 +3,7 @@
 import React, { useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { BackpackWalletAdapter, PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {  PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 
